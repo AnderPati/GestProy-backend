c37e4b8d1b39914c654a2a2c3bfe935379ca3d28
AnderPati <a_patino@fpzornotza.com>
2025-02-26 15:55:39 +0100

    sanctum en funcionamiento

--- GP-back/routes/api.php
+++ GP-back/routes/api.php
@@ -2,6 +2,36 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['Las credenciales son incorrectas.'],
+        ]);
+    }
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'user' => $user
+    ]);
+});
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $request->user()->tokens()->delete();
+    return response()->json(['message' => 'Sesión cerrada con éxito']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
